be7490495d76f4c5bbe237e6ef1fde1d488147b7
Martin Dimitrov <martivr98@gmail.com>
2023-11-16 21:51:16 +0200

    .

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ function animate() {
           });
           //change background particle colors
           backgroundParticles.forEach((backgroundParticle) => {
-            gsap.to(backgroundParticle, {
+            gsap.set(backgroundParticle, {
               color: "white",
               alpha: 1,
             });
